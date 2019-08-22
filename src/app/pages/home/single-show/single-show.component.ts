df629539872673552b97a47c9611be48da1a0997
Muhammad Abdullah <mabdulah596@hotmail.com>
2019-08-23 02:16:26 +0500

    update

--- src/app/pages/home/single-show/single-show.component.ts
+++ src/app/pages/home/single-show/single-show.component.ts
@@ -9,45 +9,53 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./single-show.component.scss']
 })
 export class SingleShowComponent implements OnInit {
-showId;
-allEpisodes:Array<any>=[];
-showDetail;
-seasons;
-cast;
-crew;
+  showId;
+  allEpisodes: Array<any> = [];
+  showDetail;
+  seasons;
+  cast;
+  crew;
 
-  constructor(private api:ApiService,private route:ActivatedRoute) {
-    this.showId=this.route.snapshot.paramMap.get('id');
+  constructor(private api: ApiService, private route: ActivatedRoute) {
+    this.showId = this.route.snapshot.paramMap.get('id');
     // console.log(this.showId);
     // this.getShow(this.showId);
-   }
+  }
 
   ngOnInit() {
     this.getShow(this.showId)
   }
-  getShow(id){
-this.api.getSingleShow(id).subscribe(res=>{
-  console.log(res);
-  this.showDetail=res;
-  this.getShowAKA(this.showId);
-  this.getShowCast(this.showId);
-  this.getShowCrew(this.showId);
-  this.getShowSeasons(this.showId);
-})
-  }
-  getShowSeasons(id){
-    this.allEpisodes=[];
-    this.api.getShowSeasons(id).subscribe((res:any)=>{
+  getShow(id) {
+    this.api.getSingleShow(id).subscribe(res => {
       console.log(res);
-      this.seasons=res;
-      this.seasons.map((elem)=>{
-        if(elem.image==null){
-          return elem ['image']='../../../../assets//img/dummy.jpg';
+      this.showDetail = res;
+      this.getShowAKA(this.showId);
+      this.getShowCast(this.showId);
+      this.getShowCrew(this.showId);
+      this.getShowSeasons(this.showId);
+    })
+  }
+  getShowSeasons(id) {
+    this.allEpisodes = [];
+    this.api.getShowSeasons(id).subscribe((res: any) => {
+      console.log(res);
+      this.seasons = res;
+      this.seasons.map((elem) => {
+        if (elem.image == null) {
+          return elem['image'] = '../../../../assets//img/dummy.jpg';
         }
       })
       res.forEach(elements => {
-        this.api.getSeasonEpisodes(elements.id).subscribe((resp:any)=>{
+        this.api.getSeasonEpisodes(elements.id).subscribe((resp: any) => {
           console.log(resp);
+          resp.map((elem) => {
+            if (elem.image == null) {
+              return elem['images'] = '../../../../assets//img/dummy.jpg';
+            }
+            else{
+              return elem['images'] = elem.image.original;
+            }
+          })
           resp.forEach(element => {
             this.allEpisodes.push(element);
           });
@@ -56,25 +64,37 @@ this.api.getSingleShow(id).subscribe(res=>{
 
       });
       console.log(this.allEpisodes);
-    })
-      }
-      getShowCast(id){
-        this.api.getShowCast(id).subscribe(res=>{
-          console.log(res);
-          this.cast=res;
-        })
-          }
-          getShowCrew(id){
-            this.api.getShowCrew(id).subscribe(res=>{
-              console.log(res);
-              this.crew=res;
-            })
-              }
-              getShowAKA(id){
-                this.api.getShowAKA(id).subscribe(res=>{
-                  console.log(res);
-                })
-                  }
 
+
+    })
+  }
+  getShowCast(id) {
+    this.api.getShowCast(id).subscribe(res => {
+      console.log(res);
+      this.cast = res;
+    })
+  }
+  getShowCrew(id) {
+    this.api.getShowCrew(id).subscribe(res => {
+      console.log(res);
+      this.crew = res;
+      this.crew.map((elem) => {
+        if (elem.person.image == null) {
+          return elem['image'] = '../../../../assets//img/user.png';
+        }
+        else{
+          return elem['image'] = elem.person.image.original;
+        }
+      })
+    })
+  }
+  getShowAKA(id) {
+    this.api.getShowAKA(id).subscribe(res => {
+      console.log(res);
+    })
+  }
+  showPersonDetails(id) {
+
+  }
 
 }
