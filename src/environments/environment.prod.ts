e86f6bc4b4594bfecc00f7eb34e37d0c7ce248ca
Muhammad Abdullah <mabdulah596@hotmail.com>
2019-08-24 00:43:58 +0500

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDj7BfG0GBry9SQWVAt6zmbnHXGpPT1a_M",
+    authDomain: "demotvapp-8d9ba.firebaseapp.com",
+    databaseURL: "https://demotvapp-8d9ba.firebaseio.com",
+    projectId: "demotvapp-8d9ba",
+    storageBucket: "demotvapp-8d9ba.appspot.com",
+    messagingSenderId: "12683513888",
+    appId: "1:12683513888:web:883b3a978f981bec"
+  }
 };
